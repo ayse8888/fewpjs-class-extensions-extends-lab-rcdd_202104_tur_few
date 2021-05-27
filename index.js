4268ccf4cade2cbecdede8ebfea38467322966cf
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-05-27 16:02:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,17 +38,24 @@ class Square extends Polygon {
      const side2 = this.arr[1];
      const side3 = this.arr[2];
      const side4 = this.arr[3];
+<<<<<<< HEAD
      if(side1 === side2 && side1 === side3  && side1 === side4 && side2 === side3 && side2 === side4 && side3 === side4) {
+=======
+     if(side1 == side2 && side1 == side3  && side1 == side4 && side2 == side3 && side2 == side4 && side3 == side4) {
+>>>>>>> 3f17fe52c7385e0bfb4b4e15e5bcfdb9321917f6
        return true
      } else {
        return false
      }
   }
+<<<<<<< HEAD
   
    get area() {
      const side1 = this.arr[0];
      return side1 * side1
   }
+=======
+>>>>>>> 3f17fe52c7385e0bfb4b4e15e5bcfdb9321917f6
 }
 
 
